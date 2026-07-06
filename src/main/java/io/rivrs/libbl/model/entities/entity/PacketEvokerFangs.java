b5af35d04f0a324e9a8dd01100894419aaee6d20
vadim-soude <vadimsoude@gmail.com>
2026-07-06 11:05:17 +0200

    feat : implement PacketEvokerFangs entity with constructors

--- src/main/java/io/rivrs/libbl/model/entities/entity/PacketEvokerFangs.java
+++ src/main/java/io/rivrs/libbl/model/entities/entity/PacketEvokerFangs.java
@@ -1,5 +1,22 @@
 package io.rivrs.libbl.model.entities.entity;
 
-public class PacketEvokerFangs {
-    // TODO: Implement PacketEvokerFangs entity
+import io.rivrs.libbl.model.entities.PacketEntity;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+
+import java.util.UUID;
+
+public class PacketEvokerFangs extends PacketEntity {
+
+    public PacketEvokerFangs(Location location) {
+        super(EntityType.EVOKER_FANGS, location);
+    }
+
+    public PacketEvokerFangs(UUID uniqueId, Location location) {
+        super(uniqueId, EntityType.EVOKER_FANGS, location);
+    }
+
+    public PacketEvokerFangs(int id, UUID uniqueId, Location location) {
+        super(id, uniqueId, EntityType.EVOKER_FANGS, location);
+    }
 }
