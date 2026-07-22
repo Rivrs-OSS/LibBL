b9eaeaca7b2a2a407ce76c223e082e1731b70cf2
vadim-soude <vadimsoude@gmail.com>
2026-07-22 14:06:52 +0200

    feat : implement PacketDragonFireball and PacketWindCharge entities with constructors

--- src/main/java/io/rivrs/libbl/model/entities/entity/PacketDragonFireball.java
+++ src/main/java/io/rivrs/libbl/model/entities/entity/PacketDragonFireball.java
@@ -1,5 +1,22 @@
 package io.rivrs.libbl.model.entities.entity;
 
-public class PacketDragonFireball {
-    // TODO: Implement PacketDragonFireball entity
+import io.rivrs.libbl.model.entities.PacketEntity;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+
+import java.util.UUID;
+
+public class PacketDragonFireball extends PacketEntity {
+
+    public PacketDragonFireball(Location location) {
+        super(EntityType.DRAGON_FIREBALL, location);
+    }
+
+    public PacketDragonFireball(UUID uniqueId, Location location) {
+        super(uniqueId, EntityType.DRAGON_FIREBALL, location);
+    }
+
+    public PacketDragonFireball(int id, UUID uniqueId, Location location) {
+        super(id, uniqueId, EntityType.DRAGON_FIREBALL, location);
+    }
 }
--- src/main/java/io/rivrs/libbl/model/entities/entity/PacketWindCharge.java
+++ src/main/java/io/rivrs/libbl/model/entities/entity/PacketWindCharge.java
@@ -1,5 +1,22 @@
 package io.rivrs.libbl.model.entities.entity;
 
-public class PacketWindCharge {
-    // TODO: Implement PacketWindCharge entity
+import io.rivrs.libbl.model.entities.PacketEntity;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+
+import java.util.UUID;
+
+public class PacketWindCharge extends PacketEntity {
+
+    public PacketWindCharge(Location location) {
+        super(EntityType.WIND_CHARGE, location);
+    }
+
+    public PacketWindCharge(UUID uniqueId, Location location) {
+        super(uniqueId, EntityType.WIND_CHARGE, location);
+    }
+
+    public PacketWindCharge(int id, UUID uniqueId, Location location) {
+        super(id, uniqueId, EntityType.WIND_CHARGE, location);
+    }
 }
